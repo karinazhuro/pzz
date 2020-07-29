23f6e744d38a11de3e0c8367ea9acb7374ee4585
karinazhuro <44468931+karinazhuro@users.noreply.github.com>
2020-07-29 17:41:25 +0300

    Added in localStorage data

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,26 @@
+'use strict';
+
 const xhr = new XMLHttpRequest();
 const url = 'https://pzz.by/api/v1/pizzas?load=ingredients,filters&filter=meal_only:0&order=position:asc';
+// const proxyUrl = "https://cryptic-headland-94862.herokuapp.com/";
 const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-
 
 xhr.open('GET', proxyUrl + url, true);
 xhr.responseType = 'json';
 xhr.send();
 
+xhr.onerror = function () {
+		alert("Запрос не удался");
+};
+
 xhr.onload = function () {
 		let newContent = '';
+		const cart = {};
+		const content = document.getElementById('content');
+		// const basket = document.getElementsByClassName('button');
+		const basket = document.querySelector('button');
+		const order = document.getElementsByClassName('order');
+
 		if (xhr.status === 200) {
 				let responseObject = xhr.response;
 				for (let i = 0; i < responseObject.response.data.length; i++) {
@@ -18,22 +30,27 @@ xhr.onload = function () {
 						newContent += '<div class="size">' +
 										'<p class="pizza">' + 'Большая' + '</p>' +
 										'<p class="price">' + responseObject.response.data[i].big_price / 10000 + '0' +
-										'<button class="order">' + 'В корзину' + '</button>' + '</p>' +
+										'<button class="basket" data-id="' + responseObject.response.data[i].title + '">' + 'В корзину' +
+										'</button>' + '</p>' +
 										'<p class="weight">' + responseObject.response.data[i].big_weight + '</p>' +
 										'</div> '; // bigSize
 						newContent += '<div class="size">' +
 										'<p class="pizza">' + 'Стандартная' + '</p>' +
 										'<p class="price">' + responseObject.response.data[i].medium_price / 10000 + '0' +
-										'<button class="order">' + 'В корзину' + '</button>' + '</p>' +
+										'<button class="basket" data-id="' + responseObject.response.data[i].title + '">' + 'В корзину' +
+										'</button>' + '</p>' +
 										'<p class="weight">' + responseObject.response.data[i].medium_weight + '</p>' +
 										'</div> ';// mediumSize
 						newContent += '<p class="anonce">' + responseObject.response.data[i].anonce + '</p>';
 						newContent += '</div> '; // pizzas
 				}
 		}
-		document.getElementById('content').innerHTML = newContent;
-};
+		content.innerHTML = newContent;
 
-xhr.onerror = function () {
-		alert("Запрос не удался");
+		document.onclick = event => {
+				if (event.target.classList.contains('basket')) {
+						localStorage.setItem('pizza', event.target.dataset.id);
+						// addToCart(event.target.dataset.id);
+				}
+		}
 };
