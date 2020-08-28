0fbd366fd8e6ae1f2495cf84696f5deb7331ae12
karinazhuro <44468931+karinazhuro@users.noreply.github.com>
2020-08-28 17:30:16 +0300

    Realize datalist to address; style for header

--- app/js/address.js
+++ app/js/address.js
@@ -12,100 +12,104 @@
 // }(window.Element.prototype);
 
 document.addEventListener('DOMContentLoaded', function () {
-  const modalButtons = document.getElementById('open-modal');
-  const overlay = document.querySelector('.overlay-modal');
-  const closeButtons = document.querySelectorAll('.modal-close');
+		const modalButtons = document.getElementById('open-modal');
+		const overlay = document.querySelector('.overlay-modal');
+		const closeButtons = document.querySelectorAll('.modal-close');
 
-  modalButtons.addEventListener('click', function (e) {
-    e.preventDefault();
+		modalButtons.addEventListener('click', function (e) {
+				e.preventDefault();
 
-    const modalId = this.getAttribute('data-modal');
-    const modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]');
+				const modalId = this.getAttribute('data-modal');
+				const modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]');
 
-    modalElem.classList.add('active');
-    overlay.classList.add('active');
-  }); // end click
+				modalElem.classList.add('active');
+				overlay.classList.add('active');
+		}); // end click
 
-  closeButtons.forEach(function (item) {
-    item.addEventListener('click', function (e) {
-      const parentModal = this.closest('.modal');
+		closeButtons.forEach(function (item) {
+				item.addEventListener('click', function (e) {
+						const parentModal = this.closest('.modal');
 
-      parentModal.classList.remove('active');
-      overlay.classList.remove('active');
-    });
-  });
+						parentModal.classList.remove('active');
+						overlay.classList.remove('active');
+				});
+		});
 
-  document.body.addEventListener('keyup', function (e) {
-    const key = e.keyCode;
+		document.body.addEventListener('keyup', function (e) {
+				const key = e.keyCode;
 
-    if (key === 27) {
-      document.querySelector('.modal.active').classList.remove('active');
-      document.querySelector('.overlay').classList.remove('active');
-    }
-  }, false);
+				if (key === 27) {
+						document.querySelector('.modal.active').classList.remove('active');
+						document.querySelector('.overlay').classList.remove('active');
+				}
+		}, false);
 
-  overlay.addEventListener('click', function () {
-    document.querySelector('.modal.active').classList.remove('active');
-    this.classList.remove('active');
-  });
+		overlay.addEventListener('click', function () {
+				document.querySelector('.modal.active').classList.remove('active');
+				this.classList.remove('active');
+		});
 
 
 });
 
-$(document).on('click', '#sendStreet', searchStreet);
-$(document).on('click', '#sendHouse', searchHouse);
+$(document).on('input', '#street', searchStreet);
+$(document).on('focus', '#house', searchHouse);
 $(document).on('click', '#sendAddress', sendAddress);
 
 let id = '';
 let houseTitleOrder = '';
 
 async function searchStreet(e) {
-  const streetTitle = $('#street').val().toUpperCase();
-  const streetUrl = `https://pzz.by/api/v1/streets?order=title%3Aasc&search=title%3A${streetTitle}%2Ctitle%3A${streetTitle}`;
+		e.preventDefault();
 
-  e.preventDefault();
+		const streetTitle = $('#street').val();
+		const streetUrl = `https://pzz.by/api/v1/streets?order=title%3Aasc&search=title%3A${streetTitle.toUpperCase()}%2Ctitle%3A${streetTitle.toUpperCase()}`;
 
-  await fetch(streetUrl)
-    .then(function (response) {
-      response.json()
-        .then(function (obj) {
-          const data = obj.response.data;
-          for (let i = 0; i < data.length; i++) {
-            if (data[i].title.toUpperCase() === streetTitle) {
-              id = data[i].id;
-            }
-          }
-        })
-    })
+		if (streetTitle.length >= 2) {
+				await fetch(streetUrl)
+				.then(function (response) {
+						response.json()
+						.then(function (obj) {
+								const data = obj.response.data;
+								for (let i = 0; i < data.length; i++) {
+										const option = document.createElement('option');
+										option.value = data[i].title;
+										$('#datalistStreet').append(option);
+										if (data[i].title === streetTitle) {
+												id = data[i].id;
+										}
+								}
+						})
+				})
+		}
 }
 
 async function searchHouse(e) {
-  const streetOrderUrl = `https://pzz.by/api/v1/streets/${id}?order=title:asc&load=region.pizzeria`;
-  const houseTitle = $('#house').val();
+		e.preventDefault();
 
-  e.preventDefault();
+		const streetOrderUrl = `https://pzz.by/api/v1/streets/${id}?order=title:asc&load=region.pizzeria`;
 
-  await fetch(streetOrderUrl)
-    .then(function (response) {
-      response.json()
-        .then(function (obj) {
-          const data = obj.response.data;
-          for (let i = 0; i < data.length; i++) {
-            if (houseTitle === data[i].title) {
-              houseTitleOrder = data[i].id;
-            }
-          }
-        })
-    })
+		await fetch(streetOrderUrl)
+		.then(function (response) {
+				response.json()
+				.then(function (obj) {
+						const data = obj.response.data;
+						for (let i = 0; i < data.length; i++) {
+								const option = document.createElement('option');
+								option.value = data[i].title;
+								$('#datalistHouse').append(option);
+						}
+				})
+		})
 }
 
 async function sendAddress(e) {
-  const houseUrl = `https://pzz.by/api/v1/house/resolve-pizzeria/${houseTitleOrder}`;
+		e.preventDefault();
 
-  e.preventDefault();
+		const houseUrl = `https://pzz.by/api/v1/house/resolve-pizzeria/${houseTitleOrder}`;
 
-  await fetch(houseUrl)
-    .then(function (response) {
-      response.json();
-    })
+		await fetch(houseUrl)
+		.then(function (response) {
+				response.json();
+		})
 }
